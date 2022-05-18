9154751bc7e18e3463c4cb78703b3ede240b4af3
Philip Okanlawon <f.bobo@live.com>
2022-05-17 20:10:45 -0600

    display a quote at random on refresh

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];   // use let if value changes, and const if value does not change
 
 // Show New Quote
 function newQuote() {
   // pick random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+
+  // check if author field is null, replace with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes from API
